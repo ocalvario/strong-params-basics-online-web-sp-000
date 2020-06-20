212ddbf16aa6058b8cfec3d78a58b81869f839ff
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-20 16:08:51 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,10 @@ class PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params["post"])
-	  @post.save
-	  redirect_to post_path(@post)
-	end
+    @post = Post.new(params.require(:post).permit(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
 
 	def update
 	  @post = Post.find(params[:id])
